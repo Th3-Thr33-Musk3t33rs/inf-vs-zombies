4539c3ea033d1328ce9cc9bf1ffee0f60243de1e
Andre Schaidhauer Luckmann <andre.schaidhauer.luckmann@sap.com>
2025-06-29 11:33:24 -0300

    removed comment

--- src/config.c
+++ src/config.c
@@ -7,7 +7,6 @@
 const int SELL_POS_X = (int)(750 * BASE_WIDTH_INT / BASE_WIDTH_FLOAT);
 const int SELL_POS_Y = (int)(40 * BASE_HEIGHT_INT / BASE_HEIGHT_FLOAT);
 
-// Define o tamanho da fonte base e a escala para a resolução atual.
 const int FONT_SIZE = (int)(BASE_FONT_SIZE * ((float)BASE_HEIGHT_INT / BASE_HEIGHT_FLOAT));
 
 int ReadHordesConfig(char *configPath, int hordes[MAX_HORDES], int maxHordes) {
