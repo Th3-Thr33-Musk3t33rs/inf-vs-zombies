93e01a03996c14326a7ce33045468168310c4a63
Edigueh <andreluck2003@gmail.com>
2025-07-01 10:52:09 -0300

    Commented utils.c

--- src/utils.c
+++ src/utils.c
@@ -23,11 +23,13 @@ void LoadLeaderboard(char* fileName, GameState* state) {
     FILE* arq = fopen(fileName, "rb");
 
     if (arq == NULL) {
+        // Erro na abertura do arquivo zera a entrada no leaderboard.
         for (int i = 0; i < MAX_PLAYERS_LEADERBOARD; i++) {
             strcpy(state->leaderboard[i].playerName, "000");
             state->leaderboard[i].points = 0;
         }
     } else {
+        // Leitura do arquivo binário.
         for (int i = 0; i < MAX_PLAYERS_LEADERBOARD; i++) {
             fread(state->leaderboard[i].playerName, sizeof(char), 4, arq);
             fread(&state->leaderboard[i].points, sizeof(int), 1, arq);
@@ -35,6 +37,7 @@ void LoadLeaderboard(char* fileName, GameState* state) {
 
         fclose(arq);
 
+        // Atualização da exibição do leaderboard caso o player tenha pontos suficientes.
         for (int i = 0; i < MAX_PLAYERS_LEADERBOARD - 1; i++) {
             for (int s = 0; s < MAX_PLAYERS_LEADERBOARD - 1; s++) {
                 if (state->leaderboard[s].points < state->leaderboard[s + 1].points) {
@@ -51,19 +54,17 @@ void SaveLeaderboard(char* fileName, GameState* state) {
     FILE* arq = fopen(fileName, "wb");
     bool isPlayerScoreEnough = false;
 
+    // Erro na abertura para escrita do arquivo binário.
     if (arq == NULL) return;
 
+    // Escrita no arquivo na posição respectiva a pontuação do jogador.
     for (int i = 0; i < MAX_PLAYERS_LEADERBOARD; i++) {
         if (isPlayerScoreEnough && i > 0) {
             fwrite(state->leaderboard[i - 1].playerName, sizeof(char), 4, arq);
             fwrite(&state->leaderboard[i - 1].points, sizeof(int), 1, arq);
-
-        }
-
-        else if (state->player.points > state->leaderboard[i].points && !isPlayerScoreEnough) {
+        } else if (state->player.points > state->leaderboard[i].points && !isPlayerScoreEnough) {
             fwrite(state->player.playerName, sizeof(char), 4, arq);
             fwrite(&state->player.points, sizeof(int), 1, arq);
-
             isPlayerScoreEnough = true;
         } else {
             fwrite(state->leaderboard[i].playerName, sizeof(char), 4, arq);
